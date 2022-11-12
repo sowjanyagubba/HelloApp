c2d17ad3e25cfefd9545381165a6fbf100c99c5b
sowjanyagubba <sowjanyagubba1223@gmail.com>
2022-11-12 16:46:03 +0530

    Added new line

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -7,6 +7,7 @@ namespace HelloApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to git");
         }
     }
 }
